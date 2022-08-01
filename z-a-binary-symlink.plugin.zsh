d97760b1d92bd99fe4313cc3c15cd78489a93b0f
Vladislav Doster <mvdoster@gmail.com>
2022-07-31 22:47:42 -0500

    style: format zsh to zinit convention
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- z-a-binary-symlink.plugin.zsh
+++ z-a-binary-symlink.plugin.zsh
@@ -5,29 +5,12 @@
 # Modified work Copyright (c) 2020-2021 Nicholas Serrano
 # Modified work Copyright (c) 2022 zdharma-continuum and contributors
 
-# According to the Zsh Plugin Standard:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
-
+# Zsh Plugin Standard: http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 autoload :za-lb-atclone-handler :za-lb-atdelete-handler
-
-# An empty stub to fill the help handler fields
 :za-lb-null-handler() { :; }
-
-@zinit-register-annex "z-a-linkbin" \
-  hook:atclone-50 \
-  :za-lb-atclone-handler \
-  :za-lb-null-handler \
-  "lbin|lbin''" # also register new ices
-
-@zinit-register-annex "z-a-linkbin" \
-  hook:\%atpull-50 \
-  :za-lb-atclone-handler \
-  :za-lb-null-handler
-
-@zinit-register-annex "z-a-linkbin" \
-  hook:atdelete-50 \
-  :za-lb-atdelete-handler \
-  :za-lb-null-handler
+@zinit-register-annex "z-a-linkbin" hook:atclone-50 :za-lb-atclone-handler :za-lb-null-handler "lbin|lbin''"
+@zinit-register-annex "z-a-linkbin" hook:\%atpull-50 :za-lb-atclone-handler :za-lb-null-handler
+@zinit-register-annex "z-a-linkbin" hook:atdelete-50 :za-lb-atdelete-handler :za-lb-null-handler
