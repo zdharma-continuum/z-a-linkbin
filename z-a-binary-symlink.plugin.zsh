3386f1ef54bf127b28afa3c41b39014ac4bc5376
Vladislav Doster <mvdoster@gmail.com>
2023-04-14 18:04:21 -0500

    feat: add consistent logging
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- z-a-binary-symlink.plugin.zsh
+++ z-a-binary-symlink.plugin.zsh
@@ -7,7 +7,6 @@
 
 # According to the Zsh Plugin Standard:
 # http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
-
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
@@ -15,14 +14,15 @@
 
 autoload :za-lb-atclone-handler :za-lb-atdelete-handler
 
-# An empty stub to fill the help handler fields
-:za-lb-null-handler() { :; }
+:za-lb-null-handler () {
+    :
+}
 
 @zinit-register-annex "z-a-linkbin" \
   hook:atclone-50 \
   :za-lb-atclone-handler \
   :za-lb-null-handler \
-  "lbin|lbin''" # also register new ices
+  "lbin|lbin''"
 
 @zinit-register-annex "z-a-linkbin" \
   hook:\%atpull-50 \
@@ -33,4 +33,3 @@ autoload :za-lb-atclone-handler :za-lb-atdelete-handler
   hook:atdelete-50 \
   :za-lb-atdelete-handler \
   :za-lb-null-handler
-
